6edfc90431653ab48490ef8718fcdbcd85b327c4
Jonathan Walters <jon@a3online.com>
2015-10-22 15:33:44 -0500

    Handling support for create/update of supporters, and making it compatible with at least PHP 5.3.29 or newer

--- Salsa.php
+++ Salsa.php
@@ -6,8 +6,16 @@ class Salsa {
 
 	private static $instance;
 
+	const RECEIVE_EMAIL_SUBSCRIBED = 3;
+	const RECEIVE_EMAIL_UNSUBSCRIBED = -3;
+	const RECEIVE_EMAIL_IMPORT = 1;
+	const RECEIVE_EMAIL_DOUBLE_OPT = 10;
+	const RECEIVE_EMAIL_NO_ACTIVITY = 15;
+
 	const AUTH_TIMEOUT = 3600; // 1 hour timeout in seconds (docs recommend 'about 2 hours')
-	const GROUPS_COLUMNS = ["groups_KEY", "organization_KEY", "chapter_KEY", "Last_Modified", "Date_Created", "Group_Name", "Reference_Name", "parent_KEY", "Description", "Notes", "Display_To_User_BOOLVALUE", "Display_To_User", "Listserve_Type", "Subscription_Type", "Manager", "Moderator_Emails", "Subject_Prefix", "Listserve_Responses", "Append_Header", "Append_Footer", "Custom_Headers", "Listserve_Options", "external_ID", "From_Email", "From_Name", "Reply_To", "Headers_To_Remove", "Confirmation_Message", "Auto_Update_BOOLVALUE", "Auto_Update", "query_KEY", "Smart_Group_Options", "Smart_Group_Error", "enable_statistics_BOOLVALUE", "enable_statistics", "join_email_trigger_KEYS", "add_to_chapter_KEY", "salesforce_id"];
+	// apparently only PHP 5.6+ can have arrays as constants...
+	public static $GROUPS_COLUMNS = array("groups_KEY", "organization_KEY", "chapter_KEY", "Last_Modified", "Date_Created", "Group_Name", "Reference_Name", "parent_KEY", "Description", "Notes", "Display_To_User_BOOLVALUE", "Display_To_User", "Listserve_Type", "Subscription_Type", "Manager", "Moderator_Emails", "Subject_Prefix", "Listserve_Responses", "Append_Header", "Append_Footer", "Custom_Headers", "Listserve_Options", "external_ID", "From_Email", "From_Name", "Reply_To", "Headers_To_Remove", "Confirmation_Message", "Auto_Update_BOOLVALUE", "Auto_Update", "query_KEY", "Smart_Group_Options", "Smart_Group_Error", "enable_statistics_BOOLVALUE", "enable_statistics", "join_email_trigger_KEYS", "add_to_chapter_KEY", "salesforce_id");
+	public static $SUPPORTER_COLUMNS = array("supporter_KEY", "organization_KEY", "chapter_KEY", "Last_Modified", "Date_Created", "Title", "First_Name", "MI", "Last_Name", "Suffix", "Email", "Password", "Receive_Email", "Email_Status", "Email_Preference", "Soft_Bounce_Count", "Hard_Bounce_Count", "Last_Bounce", "Receive_Phone_Blasts_BOOLVALUE", "Receive_Phone_Blasts", "Phone", "Cell_Phone", "Phone_Provider", "Work_Phone", "Pager", "Home_Fax", "Work_Fax", "Street", "Street_2", "Street_3", "City", "State", "Zip", "PRIVATE_Zip_Plus_4", "County", "District", "Country", "Latitude", "Longitude", "Organization", "Department", "Occupation", "Instant_Messenger_Service", "Instant_Messenger_Name", "Web_Page", "Alternative_Email", "Other_Data_1", "Other_Data_2", "Other_Data_3", "Notes", "Source", "Source_Details", "Source_Tracking_Code", "Tracking_Code", "Status", "uid", "Timezone", "Language_Code", "salesforce_id", "append_run_date", "salsa_deleted_BOOLVALUE", "salsa_deleted");
 
 	private $salsaNode;
 	private $orgKey;
@@ -56,18 +64,23 @@ class Salsa {
 	public function createGroup($attr) {
 		$params['object'] = "groups";
 		foreach($attr AS $k => $v) {
-			if(in_array($k, $this::GROUPS_COLUMNS)) {
+			if(in_array($k, self::$GROUPS_COLUMNS)) {
 				$params[$k] = $v;
 			} else {
 				error_log("Attribute '$k' is not a valid groups column name");
 			}
 		}
-		$params['Group_Name'] = $this->filterGroupName($params['Group_Name']);
-		if($this->validGroupName($params['Group_Name']) !== false) {
-			$url = $this->apiHost . "save?" . $this->buildParamString($params) . "&" . $this->format;
-			return $this->exec($url);
+		if(isset($params['groups_KEY'])) {
+			error_log('Use updateGroup when you know the group key.');
+			return json_decode('{"result":"error","messages":["There was an internal error, please try again"]}');
 		} else {
-			return json_decode('{"status":"failure","message":"Pre-existing Group_Name or no Group_Name was provided"}');
+			$params['Group_Name'] = $this->filterGroupName($params['Group_Name']);
+			if($this->validGroupName($params['Group_Name']) !== false) {
+				$url = $this->apiHost . "save?" . $this->buildParamString($params) . "&" . $this->format;
+				return $this->exec($url);
+			} else {
+				return json_decode('{"result":"error","messages":["Pre-existing Group_Name or no Group_Name was provided"]}');
+			}
 		}
 	}
 
@@ -106,7 +119,7 @@ class Salsa {
 		$params['object'] = "groups";
 		$params['key'] = $key;
 		foreach($attr AS $k => $v) {
-			if(in_array($k, $this::GROUPS_COLUMNS)) {
+			if(in_array($k, self::$GROUPS_COLUMNS)) {
 				$params[$k] = $v;
 			} else {
 				error_log("Attribute '$k' is not a valid groups column name");
@@ -119,7 +132,7 @@ class Salsa {
 			$url = $this->apiHost . "save?" . $this->buildParamString($params) . "&" . $this->format;
 			return $this->exec($url);
 		} else {
-			return json_decode('{"status":"failure","message":"Can not rename group to pre-existing Group_Name"}');
+			return json_decode('{"result":"error","messages":["Can not rename group to pre-existing Group_Name"]}');
 		}
 	}
 
@@ -164,11 +177,48 @@ class Salsa {
 	/*** END GROUP CRUD ***/
 
 	/*** SUPPORTER CRUD ***/
-	public function createSupporter($attr) {
-		return false;
+	public function createSupporter($attr, $requireEmail = true, $allowDuplicateEmails = false) {
+		$params['object'] = "supporter";
+		foreach($attr AS $k => $v) {
+			if(in_array($k, self::$SUPPORTER_COLUMNS)) {
+				$params[$k] = $v;
+			} else {
+				error_log("Attribute '$k' is not a valid groups column name");
+			}
+		}
+		if(isset($params['supporter_KEY'])) {
+			error_log('Use updateSupporter when you know the supporter key.');
+			return json_decode('{"result":"error","messages":["There was an internal error, please try again"]}');
+		} else if($requireEmail && !isset($params['Email'])) {
+			return json_decode('{"result":"error","messages":["Please input an email address"]}');
+		} else if (isset($params['Email']) && !$this->isValidEmail($params['Email'])) {
+			return json_decode('{"result":"error","messages":["Please input a valid email address"]}');
+		} else if (isset($params['Email']) && !$allowDuplicateEmails && !$this->isUniqueEmail($params['Email'])) {
+			return json_decode('{"result":"error","messages":["That email address already exists"]}');
+		} else if (!isset($params['First_Name']) || !isset($params['Last_Name']) || empty($params['First_Name']) || empty($params['Last_Name'])) {
+			return json_decode('{"result":"error","messages":["Please input both a first and last name"]}');
+		} else {
+			$url = $this->apiHost . "save?" . $this->buildParamString($params) . "&" . $this->format;
+			return $this->exec($url);
+		}
 	}
 
 	public function getSupporter($supporterKey) {
+		$params['object'] = "supporter";
+		$params['key'] = $supporterKey;
+		$url = $this->apiHost . "api/getObject.sjs?" . $this->buildParamString($params) . "&" . $this->format;
+		return $this->exec($url);
+	}
+
+	public function getSupporterByEmail($email) {
+		$params['object'] = "supporter";
+		$params['condition'] = "Email=" . $email;
+		$params['limit'] = 1;
+		$url = $this->apiHost . "api/getObjects.sjs?" . $this->buildParamString($params) . "&" . $this->format;
+		$groups = $this->exec($url);
+		if(count($groups) > 0) {
+			return $groups[0];
+		}
 		return null;
 	}
 
@@ -201,7 +251,24 @@ class Salsa {
 	}
 
 	public function updateSupporter($key, $attr) {
-		return false;
+		if($key == 0) {
+			return $this->createSupporter($attr);
+		}
+		$params['object'] = "supporter";
+		$params['key'] = $key;
+		foreach($attr AS $k => $v) {
+			if(in_array($k, self::$SUPPORTER_COLUMNS)) {
+				$params[$k] = $v;
+			} else {
+				error_log("Attribute '$k' is not a valid supporter column name");
+			}
+		}
+		if(isset($params['Email']) && (!$this->isValidEmail($params['Email']) || !$this->isUniqueEmail($params['Email']))) {
+			return json_decode('{"result":"error","messages":["This an invalid email address or the address already exists"]}');
+		} else {
+			$url = $this->apiHost . "save?" . $this->buildParamString($params) . "&" . $this->format;
+			return $this->exec($url);
+		}
 	}
 
 	public function deleteSupporter($key) {
@@ -219,6 +286,20 @@ class Salsa {
 			}
 		}
 	}
+
+	public function isValidEmail($email) {
+		return filter_var($email, FILTER_VALIDATE_EMAIL);
+	}
+
+	public function isUniqueEmail($email, $supporterKey = 0) {
+		$supporter = $this->getSupporterByEmail($email);
+		if(null === $supporter || $supporter->supporter_KEY == $supporterKey) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	/*** END SUPPORTER CRUD ***/
 
 	/***** GETTERS/SETTERS *****/
